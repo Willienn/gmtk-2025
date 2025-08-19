98f7d4fa883fc74b0cdbebe796131d1f1df5cd64
willien <willien_dev@outlook.com>
2025-08-19 17:39:54 -0300

    refactor(main): improve jumper

--- World Objects/Traps/jumper.gd
+++ World Objects/Traps/jumper.gd
@@ -7,16 +7,16 @@ const HITBOX_SPEED := 10.0
 @onready var animated_sprite: AnimatedSprite2D = $"Animated Sprite"
 @onready var hitbox: CollisionShape2D = $CollisionShape2D
 
-enum impulse_directions { UP, DOWN, LEFT, RIGHT }
+enum direction { UP, DOWN, LEFT, RIGHT }
 
 const IMPULSE_VECTORS := {
-	impulse_directions.UP: Vector2(0, -1),
-	impulse_directions.DOWN: Vector2(0, 1),
-	impulse_directions.LEFT: Vector2(-1, 0),
-	impulse_directions.RIGHT: Vector2(1, 0),
+	direction.UP: Vector2.UP,
+	direction.DOWN: Vector2.DOWN,
+	direction.LEFT: Vector2.LEFT,
+	direction.RIGHT: Vector2.RIGHT,
 }
 
-@export var direction := impulse_directions.UP
+@export var impulse_directions := direction.UP
 
 
 func _ready() -> void:
@@ -36,7 +36,7 @@ func _on_area_2d_body_exited(body: Node2D) -> void:
 
 func _process(delta: float) -> void:
 	if should_impulse and animated_sprite.frame == 1 and target_body:
-		var dir: Vector2 = IMPULSE_VECTORS[direction]
+		var dir: Vector2 = IMPULSE_VECTORS[impulse_directions]
 
 		# você pode ajustar a força separadamente para X e Y
 		if dir.y != 0:
